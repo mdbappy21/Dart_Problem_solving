74f01dfb7efd7aa6cfd7445616657e3f714923ee
Md Bappy <mdbappy21.cse@gmail.com>
2024-02-14 01:24:06 +0600

    Problem solving

--- Practice/inputUser.dart
+++ Practice/inputUser.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 
-void main(){
- // print("Enter your Name : ");//with line break.
-  stdout.write("Enter your name : ");//without line break.
-  String? name=stdin.readLineSync();//take input.?use for null safety.
-  print("Your name is :$name");//$ use for printing variable.
+void main() {
+  // print("Enter your Name : ");//with line break.
+  stdout.write("Enter your name : "); //without line break.
+  String? name = stdin.readLineSync(); //take input.?use for null safety.
+  print("Your name is :$name"); //$ use for printing variable.
+
+  print("Enter two name that seperated by space");
+  List<String> inputs =stdin.readLineSync().split(' '); //two input seperated by Space
+  String Sheldon = inputs[0];
+  String Raj = inputs[1];
+  print("Sheldon :$Sheldon    Raj: $Raj");
 }
