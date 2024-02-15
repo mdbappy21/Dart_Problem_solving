e1a860a155331d714fac209a83d2a473a4f661a9
Md Bappy <mdbappy21.cse@gmail.com>
2024-02-16 02:00:36 +0600

    Problem solving

--- Problem Solving/Beecrowd/1958_Scientific Notation.dart
+++ Problem Solving/Beecrowd/1958_Scientific Notation.dart
@@ -3,21 +3,19 @@ import 'dart:io';
 void main() {
   double X = double.parse(stdin.readLineSync()!);
   String s = X.toStringAsExponential(4).toUpperCase();
-  
+
   List<String> parts = s.split('E');
   String first = parts[0];
   String second = parts[1];
-  int len=second.length;
-  if(len<3)
-  {
-    String a=second[0];
-    String b=second[1];
-    second='E'+a+'0'+b;
-  }
+  int len = second.length;
+  if (len < 3) {
+    String a = second[0];
+    String b = second[1];
+    second = 'E' + a + '0' + b;
+  } else
+    second = 'E' + second;
+  if (first[0] == '-')
+    print("$first$second");
   else
-  second='E'+second;
-  if(first[0]=='-')
-  print("$first$second");
-  else
-  print("+$first$second");
+    print("+$first$second");
 }
