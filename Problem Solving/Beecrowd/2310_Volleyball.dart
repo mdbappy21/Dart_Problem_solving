0328ad5b5717b49d5fc83923f66b3cf07cb62448
Md Bappy <mdbappy21.cse@gmail.com>
2024-02-26 23:51:13 +0600

    Problem solving

--- Problem Solving/Beecrowd/2310_Volleyball.dart
+++ Problem Solving/Beecrowd/2310_Volleyball.dart
@@ -5,18 +5,16 @@ void main() {
   double sum_s = 0.00, sum_b = 0.00, sum_a = 0.00;
   double sum_s1 = 0.00, sum_b1 = 0.00, sum_a1 = 0.00;
   double ans_s, ans_b, ans_a;
-  
+  String name;
   n = int.parse(stdin.readLineSync()!);
 
-  for (i = 1; i <= n; i++) {  
-    String name = stdin.readLineSync()!;
-
+  for (i = 1; i <= n; i++) {
+    name = stdin.readLineSync()!;
     List<int> values = stdin.readLineSync()!.split(' ').map(int.parse).toList();
     s = values[0];
     b = values[1];
     a = values[2];
 
-  
     List<int> values1 = stdin.readLineSync()!.split(' ').map(int.parse).toList();
     s1 = values1[0];
     b1 = values1[1];
@@ -33,8 +31,8 @@ void main() {
   ans_s = (sum_s1 / sum_s) * 100.00;
   ans_b = (sum_b1 / sum_b) * 100.00;
   ans_a = (sum_a1 / sum_a) * 100.00;
-
-  print("Pontos de Saque: ${ans_s.toStringAsFixed(2)} %.");
+  name = "Pontos de";
+  print("$name Saque: ${ans_s.toStringAsFixed(2)} %.");
   print("Pontos de Bloqueio: ${ans_b.toStringAsFixed(2)} %.");
   print("Pontos de Ataque: ${ans_a.toStringAsFixed(2)} %.");
 }
